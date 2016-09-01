4639cf0d816f33bc67ace690d74115ca30dc8bd8
Max Timkovich <mtimkovich@gmail.com>
2016-09-01 05:21:59 -0700

    calculate elo for sets

--- smash.py
+++ smash.py
@@ -11,7 +11,27 @@ class Set:
         self.entrant1Score = match['entrant1Score']
         self.entrant2Score = match['entrant2Score']
 
+    # Calculate the rating for each game in the set
+    def rate(self, one, two):
+        for _ in range(self.entrant1Score):
+            one.rating, two.rating = trueskill.rate_1vs1(one.rating, two.rating)
+        for _ in range(self.entrant2Score):
+            two.rating, one.rating = trueskill.rate_1vs1(two.rating, one.rating)
 
+        return one, two
+
+
+# TODO: Map ids to player tags
+class Player:
+    def __init__(self, id):
+        self.id = 0
+        self.rating = trueskill.Rating()
+
+    def expose(self):
+        return trueskill.expose(self.rating)
+
+
+# This should be capitalized, but jokes are more important than style
 class gg:
     def __init__(self, tournament):
         self.tournament = tournament
@@ -30,6 +50,20 @@ class gg:
 
 if __name__ == '__main__':
     g = gg('get-smashed-at-the-foundry-101')
-    sets = g.sets
 
-    print sets[0].entrant1
+    # TODO: Move this to method
+    players = {}
+
+    for s in g.sets:
+        if s.entrant1 not in players:
+            players[s.entrant1] = Player(s.entrant1)
+        if s.entrant2 not in players:
+            players[s.entrant2] = Player(s.entrant2)
+
+        one = players[s.entrant1]
+        two = players[s.entrant2]
+
+        one, two = s.rate(one, two)
+
+    for id, player in sorted(players.items(), key=lambda x: x[1].expose()):
+        print id, ':', trueskill.expose(player.rating)
