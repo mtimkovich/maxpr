7ac977648b4d46a162bb8f29a9b54ec1f3a92473
Max Timkovich <mtimkovich@gmail.com>
2016-10-20 11:40:24 -0700

    updated main file

--- maxpr.py
+++ maxpr.py
@@ -6,5 +6,7 @@ smash_gg = smash.gg('get-smashed-at-the-foundry-101')
 players = {}
 smash_gg.calc_elo(players)
 
-for name, player in sorted(players.items(), key=lambda x: x[1].expose()):
-    print name, ':', player.expose()
+i = 1
+for name, player in sorted(players.items(), key=lambda x: x[1].expose(), reverse=True):
+    print '{: >3} {: >20} {: >20}'.format(i, name, player.expose())
+    i += 1
